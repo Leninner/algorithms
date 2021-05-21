e1fb8d185fe763f988aab530335ac11f0bdbbadc
Leninner <Mazabandalenin180@gmail.com>
2021-05-21 15:56:06 -0500

    Agregamos mini-max sum, solución al ejercicio de Hackerrank

---
+++ mini-maxSum.js
@@ -0,0 +1,28 @@
+function miniMaxSum(arr) {
+    let valor = 0;
+    let max = 0;
+    let y = 0;
+    let arreglo = [];
+    while (y < arr.length) {
+        valor = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (i != y) {
+                valor += arr[i];
+            }
+        }
+        if (valor > max) {
+            max = valor;
+        }
+        arreglo.push(valor);
+        y++;
+    }
+    let min = arreglo[0];
+    for (let i = 0; i < arreglo.length; i++) {
+        if (arreglo[i] <= min) {
+            min = arreglo[i];
+        }
+    }
+    console.log(min, max);
+}
+
+miniMaxSum([1, 3, 5, 7, 9]);
