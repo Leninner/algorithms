772d6527b89b522b9c84b730c258b08426054361
Leninner <Mazabandalenin180@gmail.com>
2021-05-31 09:51:04 -0500

    Añado la solución a Divisible Sum Pairs de Hackerrank

---
+++ divisibleSumPairs.js
@@ -0,0 +1,15 @@
+function divisibleSumPairs(n, k, ar) {
+    let con = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i < j && (ar[i] + ar[j]) % k == 0) {
+                con++;
+            }
+        }
+    }
+    console.log(con);
+    return con;
+}
+
+divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]);
+divisibleSumPairs(6, 5, [1, 2, 3, 4, 5, 6]);
