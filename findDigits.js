14bcb56eb024b8c5273cc368110c4ca4bcbec684
Leninner <mazabandalenin180@gmail.com>
2021-06-28 21:21:03 -0500

    Añado la solución a Fin Digits de Hackerrank

---
+++ findDigits.js
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/find-digits/problem
+
+function findDigits(n) {
+    // Write your code here
+    let float = Array.from(n.toString()),
+        cont = 0;
+    float.forEach((elemento) => {
+        if (n % elemento == 0) {
+            cont++;
+        }
+    });
+    return cont;
+}
+
+findDigits(10); // 1
+findDigits(111); // 3
+findDigits(124); // 3
+findDigits(12); // 2
+findDigits(1012); //3
