cc3b32081f7053160f4480705cc332d5a02775c3
Leninner <mazabandalenin180@gmail.com>
2021-07-15 22:11:04 -0500

    Añado solución a DNA complementario de Codewars

---
+++ complementaryDNA.js
@@ -0,0 +1,26 @@
+// Complemento de A = T
+// Complemento de T = A
+// Complemento de G = C
+// Complemento de C = G
+
+function DNAStrand(dna) {
+    let arregloDNA = [];
+
+    dna.split("").forEach((element) => {
+        if (element == "A") {
+            arregloDNA.push("T");
+        } else if (element == "T") {
+            arregloDNA.push("A");
+        } else if (element == "G") {
+            arregloDNA.push("C");
+        } else {
+            arregloDNA.push("G");
+        }
+    });
+
+    return arregloDNA.join("");
+}
+
+console.log(DNAStrand("AAAA")); // TTTT
+console.log(DNAStrand("ATTGC")); // TAACG
+console.log(DNAStrand("GTAT")); // CATA
