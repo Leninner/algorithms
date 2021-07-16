ecc4428c0f790368824599b7967cd40233200ab8
Leninner <mazabandalenin180@gmail.com>
2021-07-15 21:58:15 -0500

    Añado una nueva solución a contar vocales

--- contarVocales.js
+++ contarVocales.js
@@ -1,18 +1,18 @@
 // Encontrar el número de letras encontradas utilizando expresiones regulares
 
 function getCount(str) {
-    let vowelsCount = 0;
-
-    let cadena = str;
     let expresion = /[a,e,i,o,u]/gi;
-    let array_emparejamientos = cadena.match(expresion);
+    let array_emparejamientos = str.match(expresion);
 
     if (!array_emparejamientos) {
         return 0;
     } else {
-        vowelsCount = array_emparejamientos.length;
-        return vowelsCount;
+        return array_emparejamientos.length;
     }
 }
+
+// function getCount(str) {
+//     return str.replace(/[^aeiou]/gi, '').length;
+//   }
 
 console.log(getCount("my pyx")); // 0
