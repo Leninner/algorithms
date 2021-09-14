bf6be3e6b81cf2b0ab2a6616864b33700667b314
Leninner <mazabandalenin180@gmail.com>
2021-09-13 21:01:22 -0500

    Añado ejercicio de Bettatech

---
+++ contarPalabras.js
@@ -0,0 +1,21 @@
+// Programa que recopila la cantidad de palabras de un string.
+
+const normalizar = (word) => {
+  return word.toLowerCase().replace(/[.|,|;]/g, "");
+};
+const contarPalabras = (string) => {
+  let vocales = {};
+  let arrPalabras = string.split(" ");
+
+  arrPalabras.forEach((element) => {
+    if (normalizar(element) in vocales) {
+      vocales[normalizar(element)]++;
+    } else {
+      vocales[normalizar(element)] = 1;
+    }
+  });
+
+  console.table(vocales);
+};
+
+contarPalabras("Lenin, estás re pro, bett, lenin, marquitos, ala ala lus luz, estas");
