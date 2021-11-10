39911b79d0c3b572762ac49b757bd55a5455aef1
leninner <mazabandalenin180@gmail.com>
2021-11-09 21:29:20 -0500

    Update

--- code/dayOfTheProgrammer.js
+++ code/dayOfTheProgrammer.js
@@ -1,58 +1,36 @@
 function dayOfProgrammer(year) {
-    if (year >= 1700 && year <= 1917) {
-        if (year % 4 == 0) {
-            console.log(
-                ((366 - 256) / ((366 - 256) / 12).toFixed()).toFixed() +
-                    "." +
-                    "0" +
-                    ((365 - 256) / 12).toFixed() +
-                    "." +
-                    year
-            );
-        } else {
-            console.log(
-                ((333 - 256) / ((333 - 256) / 12).toFixed()).toFixed() +
-                    "." +
-                    "0" +
-                    ((365 - 256) / 12).toFixed() +
-                    "." +
-                    year
-            );
-        }
-    } else if (year >= 1918 && year <= 2700) {
-        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
-            console.log(
-                ((366 - 256) / ((366 - 256) / 12).toFixed()).toFixed() +
-                    "." +
-                    "0" +
-                    ((365 - 256) / 12).toFixed() +
-                    "." +
-                    year
-            );
-        } else {
-            if (year == 1918) {
-                console.log(
-                    parseInt(
-                        ((333 - 256) / ((333 - 256) / 12).toFixed()).toFixed()
-                    ) +
-                        13 +
-                        ".0" +
-                        ((365 - 256) / 12).toFixed() +
-                        "." +
-                        year
-                );
-            } else {
-                console.log(
-                    ((333 - 256) / ((333 - 256) / 12).toFixed()).toFixed() +
-                        "." +
-                        "0" +
-                        ((365 - 256) / 12).toFixed() +
-                        "." +
-                        year
-                );
-            }
-        }
+  if (year >= 1700 && year <= 1917) {
+    if (year % 4 == 0) {
+      console.log(
+        ((366 - 256) / ((366 - 256) / 12).toFixed()).toFixed() + '.' + '0' + ((365 - 256) / 12).toFixed() + '.' + year
+      );
+    } else {
+      console.log(
+        ((333 - 256) / ((333 - 256) / 12).toFixed()).toFixed() + '.' + '0' + ((365 - 256) / 12).toFixed() + '.' + year
+      );
     }
+  } else if (year >= 1918 && year <= 2700) {
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+      console.log(
+        ((366 - 256) / ((366 - 256) / 12).toFixed()).toFixed() + '.' + '0' + ((365 - 256) / 12).toFixed() + '.' + year
+      );
+    } else {
+      if (year == 1918) {
+        console.log(
+          parseInt(((333 - 256) / ((333 - 256) / 12).toFixed()).toFixed()) +
+            13 +
+            '.0' +
+            ((365 - 256) / 12).toFixed() +
+            '.' +
+            year
+        );
+      } else {
+        console.log(
+          ((333 - 256) / ((333 - 256) / 12).toFixed()).toFixed() + '.' + '0' + ((365 - 256) / 12).toFixed() + '.' + year
+        );
+      }
+    }
+  }
 }
 dayOfProgrammer(1918);
 dayOfProgrammer(2700);
--- code/vocalesYConsonantes.js
+++ code/vocalesYConsonantes.js
@@ -1,16 +1,16 @@
 function vowelsAndConsonants(s) {
-  const vowels = "aeiou";
-  var consonants = "";
+  const vowels = 'aeiou';
+  var consonants = '';
 
   for (var i = 0; i < s.length; i++) {
     if (vowels.includes(s[i])) {
       console.log(s[i]);
     } else {
-      consonants += s[i] + "\n";
+      consonants += s[i] + '\n';
     }
   }
 
   console.log(consonants.trim());
 }
 
-vowelsAndConsonants("Lenin Mazabanda. ¿Cómo estás?");
+vowelsAndConsonants('Lenin Mazabanda. ¿Cómo estás?');
