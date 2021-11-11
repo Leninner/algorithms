02c4fbccd6db248a3b8b5c6519f1a771ec427838
leninner <mazabandalenin180@gmail.com>
2021-11-10 19:04:51 -0500

    Añado test to Angry Professor

--- code/angryProfessor.js
+++ code/angryProfessor.js
@@ -2,15 +2,13 @@
 // k = mínima de estudiantes
 // a = array con tiempo de entrada de cada estudiante
 
-function angryProfessor(k, a) {
-    // Write your code here
-    if (a.filter((item) => item <= 0).length < k) {
-        return "YES";
-    } else {
-        return "NO";
-    }
-}
+const angryProfessor = (k, a) => {
+  // Write your code here
+  if (a.filter((item) => item <= 0).length < k) {
+    return 'YES';
+  } else {
+    return 'NO';
+  }
+};
 
-angryProfessor(3, [-2, -1, 0, 1, 2]); //NO
-angryProfessor(3, [-1, -3, 4, 2]); //YES
-angryProfessor(2, [0, -1, 2, 1]); //NO
+export { angryProfessor };
---
+++ tests/angryProfessor.test.js
@@ -0,0 +1,7 @@
+import { angryProfessor } from '../code/angryProfessor';
+
+test('El código debe funcionar correctamente', () => {
+  expect(angryProfessor(2, [0, -1, 2, 1])).toBe('NO');
+  expect(angryProfessor(4, [0, -1, 2, 1])).toBe('YES');
+  expect(angryProfessor(3, [-1, -3, 4, 2])).toBe('YES');
+});
