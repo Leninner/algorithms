b159c21a2bd7c066d66edb7000ee91b046653e4a
leninner <mazabandalenin180@gmail.com>
2021-11-26 12:01:09 -0500

    Añado solución a picking numbers

---
+++ tests/pickingNumbers.test.js
@@ -0,0 +1,7 @@
+import pickingNumbers from '../code/pickingNumbers';
+
+test('Debe entregar un resultado acertado', () => {
+  expect(pickingNumbers([4, 6, 5, 3, 3, 1])).toBe(3);
+  expect(pickingNumbers([1, 2, 2, 3, 1, 2])).toBe(5);
+  expect(pickingNumbers([1, 1, 3, 3, 4, 4, 5, 5, 5])).toBe(5);
+});
