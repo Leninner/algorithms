82feb0e7393a62cfa23c35fec69a2ebe331ae22d
leninner <mazabandalenin180@gmail.com>
2021-11-27 21:29:46 -0500

    [UPDATE] files

--- code/climbingTheLeaderboard.js
+++ code/climbingTheLeaderboard.js
@@ -26,3 +26,30 @@ function climbingLeaderboard(ranked, player) {
 }
 
 export { climbingLeaderboard };
+
+// NOTE: Solución de 12 puntos para el mismo ejercicio. Tiene problemas con el tiempo
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+
+// function climbingLeaderboard(ranked, player) {
+//   let positions = [];
+//   let newArray = [...ranked.filter((x, index) => x !== ranked[index + 1])];
+//   let minArray = [...player.filter((x, index) => x !== ranked[index + 1])];
+
+//   minArray.forEach((value) => {
+//     newArray.push(value);
+//     newArray.sort((a, b) => b - a).filter((x, index) => x !== ranked[index + 1]);
+//     positions.push(newArray.indexOf(value) + 1);
+//   });
+
+//   return positions;
+// }
+
+// console.log(climbingLeaderboard([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102]));
