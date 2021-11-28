479bceb59f3e9b19451c4e97250ac34f2987836a
leninner <mazabandalenin180@gmail.com>
2021-11-28 11:41:18 -0500

    [ADD] a new exercise about longest common prefix of leetcode

---
+++ code/longestCommonPrefix.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * @param {strs} arreglo de strings
+ * @returns {string} prefixes en común
+ */
+
+// export const longestCommonPrefix = (strs) => {
+//   return strs.reduce((acumulador, current) => {
+//     if (current === '') return '';
+//     for (let i = 0; i < acumulador.length; i++) {
+//       if (acumulador[i] !== current[i]) return current.slice(0, i);
+//     }
+//     return acumulador;
+//   });
+// };
+
+// regex for replace all numbers in a string
+// const regex = /\d+/g;
+
+export const longestCommonPrefix = (strs) => {
+  if (strs.includes('')) return '';
+
+  let obj = {};
+  let answer = true;
+  let main = strs.join('');
+
+  for (let i = 0; i < main.length; i++) {
+    if (obj[main[i]]) {
+      obj[main[i]]++;
+    } else {
+      obj[main[i]] = 1;
+    }
+  }
+
+  let arr = Object.entries(obj).filter((value) => value[1] === strs.length);
+
+  for (let i = 0; i < strs.length; i++) {
+    if (!strs[i].includes(arr[0][0])) {
+      answer = false;
+      break;
+    }
+  }
+
+  return answer ? arr.flat().join('').replace(/\d+/g, '') : '';
+};
---
+++ tests/longestCommonPrefix.test.js
@@ -0,0 +1,7 @@
+import { longestCommonPrefix } from '../code/longestCommonPrefix';
+
+test('Debe dar resultados correctos', () => {
+  expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl');
+  expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toBe('');
+  expect(longestCommonPrefix(['', 'b'])).toBe('');
+});
