5d21bb945cdcba871bb58a777209d23a8e336f22
leninner <mazabandalenin180@gmail.com>
2021-11-29 22:16:03 -0500

    [ADD] solution for CamelCaseRefactored

---
+++ code/CamelCaseRefactored.js
@@ -0,0 +1,18 @@
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+const isWithinUpperCaseLetters = (char) => {
+  if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+    return true;
+  }
+};
+
+const camelcase = (s) => {
+  return s.split('').filter(isWithinUpperCaseLetters).length + 1;
+};
+
+export default camelcase;
---
+++ tests/CamelCaseRefactored.test.js
@@ -0,0 +1,7 @@
+import camelcase from '../code/CamelCaseRefactored';
+
+test('Debe dar resultados correctos', () => {
+  expect(camelcase('saveChangesInTheEditor')).toBe(5);
+  expect(camelcase('camelCase')).toBe(2);
+  expect(camelcase('leninWithinLoveIsMoreIntuitiveThanYouThink')).toBe(9);
+});
