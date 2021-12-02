aa1d89a66c10838746b7cff677376350a4f56814
leninner <mazabandalenin180@gmail.com>
2021-12-02 08:46:35 -0500

    [UNDERSTAND] the problem

--- code/jumpingOnClouds.js
+++ code/jumpingOnClouds.js
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?isFullScreen=true
 // Complete the jumpingOnClouds function below
+
 /**
  *
  * @param {array} c => Array of clouds
@@ -7,32 +8,26 @@
  */
 
 const jumpingOnClouds = (c, k) => {
-  // let energy = 100; //initial energy
-  // let i = k % c.length; //initial jump from 0
-  // energy -= c[i] * 2 + 1; //initial energy loss
-  // while (i != 0) {
-  //   i = (i + k) % c.length; //jump
-  //   energy -= c[i] * 2 + 1;
-  // }
   let n = c.length;
-  let E = 100;
+  let energy = 100;
   let current_cloud = 0;
+
   while (true) {
     current_cloud += k;
     if (current_cloud >= n) {
       current_cloud -= n;
     }
-    E -= 1;
+    energy -= 1;
     if (c[current_cloud] == 1) {
-      E -= 2;
+      energy -= 2;
     }
     if (current_cloud == 0) {
-      console.log(E);
+      console.log(energy);
       break;
     }
   }
 
-  return E;
+  return energy;
 };
 
 export default jumpingOnClouds;
