52092a353950ad2eee3ee348c5e652ec656f49e4
leninner <mazabandalenin180@gmail.com>
2021-12-08 10:05:50 -0500

    Update

---
+++ code/convertFrac.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/54d7660d2daf68c619000d95/train/javascript/
+
+/**
+ *
+ * @param {array} lst
+ * @output {string} string
+ */
+
+// Input: [ [1, 2], [1, 3], [1, 4] ]
+// Output: (6,12)(4,12)(3,12)
+
+const convertFrac = (lst) => {};
+
+console.log(
+  convertFrac([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ])
+);
