0be6ada73438759e86c764b8490e064f5e759b78
leninner <mazabandalenin180@gmail.com>
2021-12-08 10:00:44 -0500

    [UPDATE]

---
+++ tests/isPrime.test.js
@@ -0,0 +1,8 @@
+import { isPrime } from '../code/isPrime';
+
+test('Debe dar soluciones correctas', () => {
+  expect(isPrime(2)).toBe(true);
+  expect(isPrime(3)).toBe(true);
+  expect(isPrime(4)).toBe(false);
+  expect(isPrime(21)).toBe(false);
+});
