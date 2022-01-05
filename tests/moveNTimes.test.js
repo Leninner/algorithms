0b81589af085090859cd818097a94f756ee16f96
leninner <mazabandalenin180@gmail.com>
2022-01-04 21:01:55 -0500

    Update

---
+++ tests/moveNTimes.test.js
@@ -0,0 +1,16 @@
+import { moveNTimes } from '../code/moveNTimes.js';
+
+xdescribe('Función para mover N veces una letra', () => {
+  test('Debe dar resultados correctos', () => {
+    expect(moveNTimes('a', 4)).toBe('e');
+    expect(moveNTimes('a', 0)).toBe('a');
+    expect(moveNTimes('a', -1)).toBe('z');
+    expect(moveNTimes('a', -2)).toBe('y');
+    expect(moveNTimes('a', -3)).toBe('x');
+    expect(moveNTimes('a', -4)).toBe('w');
+    expect(moveNTimes('a', -5)).toBe('v');
+    expect(moveNTimes('a', -6)).toBe('u');
+    expect(moveNTimes('z', 1)).toBe('a');
+    expect(moveNTimes('z', 2)).toBe('b');
+  });
+});
