f6c6f299956993a1f98730ba22244414fabb2bfd
leninner <mazabandalenin180@gmail.com>
2022-01-17 07:48:39 -0500

    Update

---
+++ code/stackAmazon.js
@@ -0,0 +1,43 @@
+class StackAmazon {
+  constructor() {
+    this.arreglo = [];
+  }
+
+  getArreglo() {
+    return this.arreglo;
+  }
+
+  push(value) {
+    this.arreglo.push(value);
+  }
+
+  pop() {
+    return this.arreglo.length ? this.arreglo.shift() : null;
+  }
+
+  max() {
+    return this.arreglo.length
+      ? this.arreglo.reduce((acc, curr) => {
+          if (acc < curr) {
+            acc = curr;
+          }
+
+          return acc;
+        })
+      : null;
+  }
+}
+
+const stack = new StackAmazon();
+
+stack.push(1);
+stack.push(45);
+stack.push(2);
+stack.pop();
+console.log(stack.max());
+console.log(stack.getArreglo());
+stack.pop();
+stack.pop();
+console.log(stack.pop());
+console.log(stack.getArreglo());
+console.log(stack.max());
