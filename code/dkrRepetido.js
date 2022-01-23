6841fec43f2e657fedac0400102855700368e015
leninner <mazabandalenin180@gmail.com>
2022-01-23 16:50:15 -0500

    Update

--- code/dkrRepetido.js
+++ code/dkrRepetido.js
@@ -2,24 +2,27 @@
  * The goal for this challenge is find the first repeated number in an array.
  */
 
-// Solution one
+// Solution one, Complejidad temporal: O(n^2), Complejidad espacial: O(1)
 
 const drkRepetido = (arr) => {
-  return arr.find((num, index) => arr.indexOf(num) !== index);
+  return arr.find((num, index) => arr.indexOf(num) !== index); // O(n^2)
 };
 
-console.log(
-  drkRepetido([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1,
-  ])
-);
+let initialTimeOne = performance.now();
+drkRepetido([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1,
+]);
+let finalTimeOne = performance.now();
 
-// Solution two
+console.log(`Elapsed time in solution One: ${finalTimeOne - initialTimeOne} ms`);
+
+// Solution two, Complejidad Temporal: O(n), Complejidad Espacial: O(n)
 
 const drkRepetidoDos = (arreglo) => {
   let results = {};
 
   for (let i = 0; i < arreglo.length; i++) {
+    // O(n)
     if (results[arreglo[i]]) {
       break;
     } else {
@@ -30,8 +33,9 @@ const drkRepetidoDos = (arreglo) => {
   return Object.values(results)[0];
 };
 
-console.log(
-  drkRepetidoDos([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1,
-  ])
-);
+let initialTime = performance.now();
+drkRepetidoDos([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1,
+]);
+let finalTime = performance.now();
+console.log(`Elapsed time in solution Two: ${finalTime - initialTime} ms`);
