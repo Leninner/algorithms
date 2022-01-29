80d77d9ae7b9b169e67044c16219c5eb7a208ed5
leninner <mazabandalenin180@gmail.com>
2022-01-28 20:42:50 -0500

    Added Sum of Positives for Codewars

---
+++ code/sumOfPositive.js
@@ -0,0 +1,10 @@
+const sumOfPositive = (arrayOfNumbers) =>
+  arrayOfNumbers.reduce((acc, curr) => {
+    if (!(curr < 0)) {
+      acc += curr;
+    }
+
+    return acc;
+  }, 0);
+
+console.log(sumOfPositive([1, 2, 4, -7, -8, 9]));
