14a764149b4adc50b051d739a2722a8d70ad7c6a
leninner <mazabandalenin180@gmail.com>
2022-02-04 15:01:05 -0500

    Update

--- code/turingChallenges.js
+++ code/turingChallenges.js
@@ -1,47 +1,84 @@
-export const strangeScore = (ops) => {
-  let result = [];
+// export const strangeScore = (ops) => {
+//   let result = [];
 
-  for (let i = 0; i < ops.length; i++) {
-    if (ops[i] === '+') {
-      result.push(result[result.length - 2] + result[result.length - 1]);
-    } else if (ops[i] === 'D') {
-      result.push(result[result.length - 1] * 2);
-    } else if (ops[i] === 'C') {
-      result.pop();
-    } else {
-      result.push(parseInt(ops[i]));
-    }
-  }
+//   for (let i = 0; i < ops.length; i++) {
+//     if (ops[i] === '+') {
+//       result.push(result[result.length - 2] + result[result.length - 1]);
+//     } else if (ops[i] === 'D') {
+//       result.push(result[result.length - 1] * 2);
+//     } else if (ops[i] === 'C') {
+//       result.pop();
+//     } else {
+//       result.push(parseInt(ops[i]));
+//     }
+//   }
 
-  return result.reduce((acc, curr) => acc + curr);
+//   return result.reduce((acc, curr) => acc + curr);
+// };
+
+// export const comprobateBrackets = (str) => {
+//   if (str.length % 2 !== 0) return false;
+
+//   const array = str.split('');
+//   let stack = [];
+//   let isCorrect = true;
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === '(' || array[i] === '[' || array[i] === '{') {
+//       stack.push(array[i]);
+//     } else if (
+//       (stack[stack.length - 1] === '(' && array[i] === ')') ||
+//       (stack[stack.length - 1] === '[' && array[i] === ']') ||
+//       (stack[stack.length - 1] === '{' && array[i] === '}')
+//     ) {
+//       stack.pop();
+//     } else if (stack.length === 0) {
+//       if (i !== array.length - 1) {
+//         isCorrect = false;
+//       }
+//     } else {
+//       isCorrect = false;
+//     }
+//   }
+
+//   return isCorrect;
+// };
+
+const moveNPosition = (nums, k) => {
+  const portion = nums.splice(-k);
+  return [...portion, ...nums];
 };
 
-export const comprobateBrackets = (str) => {
-  if (str.length % 2 !== 0) return false;
+console.log(moveNPosition([1, 2, 3, 4, 5, 6, 7], 3));
+console.log(moveNPosition([-1, -100, 3, 99], 2));
 
-  const array = str.split('');
-  let stack = [array[0]];
-  let isCorrect = true;
+const sumLeftLeavesInABinaryTree = (root) => {
+  if (!root) return 0;
 
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] === ']' || array[i] === '}' || array[i] === ')') return false;
+  let sum = 0;
 
-    if (array[i] === '(' || array[i] === '[' || array[i] === '{') {
-      stack.push(array[i]);
-    } else if (
-      (stack[stack.length - 1] === '(' && array[i] === ')') ||
-      (stack[stack.length - 1] === '[' && array[i] === ']') ||
-      (stack[stack.length - 1] === '{' && array[i] === '}')
-    ) {
-      stack.pop();
-    } else if (stack.length === 0) {
-      if (i !== array.length - 1) {
-        isCorrect = false;
+  const traverse = (node) => {
+    if (node.left) {
+      if (!node.left.left && !node.left.right) {
+        sum += node.left.val;
       }
-    } else {
-      isCorrect = false;
+      traverse(node.left);
     }
-  }
 
-  return isCorrect;
+    if (node.right) {
+      traverse(node.right);
+    }
+  };
+
+  traverse(root);
+
+  return sum;
 };
+
+console.log(
+  sumLeftLeavesInABinaryTree({
+    val: 1,
+    left: { val: 2, left: null, right: null },
+    right: { val: 3, left: null, right: null },
+  })
+);
