41d8dc53f1152bf195ad11c33430b75fe2a65b59
leninner <mazabandalenin180@gmail.com>
2022-02-06 15:33:26 -0500

    Update fibonacci

--- code/fibonacci.js
+++ code/fibonacci.js
@@ -15,6 +15,21 @@ const fibonacci = (n) => {
   }
 };
 
+const fibonacciRevisited = (n) => {
+  if (n === 0 || n === 1) return n;
+  let a = 1,
+    b = 0,
+    c;
+
+  for (let i = 0; i < n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
+};
+
 // calcular la serie de  fibonacci e ir almacenando  los resultados en un arreglo
 
 export const fibonacciInArray = (n) => {
@@ -31,4 +46,19 @@ export const fibonacciInArray = (n) => {
   }
 
   return array;
+};
+
+// Serie de fibonacci con recursividad
+
+const fibonacciRecursive = (n) => {
+  if (n === 0 || n === 1) return n;
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
+
+// Recursividad con memoization
+
+const fibonacciMemoization = (n, memo = []) => {
+  if (memo[n] !== undefined) return memo[n];
+  if (n === 0 || n === 1) return n;
+  return (memo[n] = fibonacciMemoization(n - 1, memo) + fibonacciMemoization(n - 2, memo));
 };
