3d0f8b1be257f9b69530705a50d196c8233d93bb
leninner <mazabandalenin180@gmail.com>
2022-02-08 12:22:10 -0500

    Update

--- code/mini-maxSum.js
+++ code/mini-maxSum.js
@@ -1,28 +1,46 @@
 function miniMaxSum(arr) {
-    let valor = 0;
-    let max = 0;
-    let y = 0;
-    let arreglo = [];
-    while (y < arr.length) {
-        valor = 0;
-        for (let i = 0; i < arr.length; i++) {
-            if (i != y) {
-                valor += arr[i];
-            }
-        }
-        if (valor > max) {
-            max = valor;
-        }
-        arreglo.push(valor);
-        y++;
+  let valor = 0;
+  let max = 0;
+  let y = 0;
+  let arreglo = [];
+  while (y < arr.length) {
+    valor = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (i != y) {
+        valor += arr[i];
+      }
     }
-    let min = arreglo[0];
-    for (let i = 0; i < arreglo.length; i++) {
-        if (arreglo[i] <= min) {
-            min = arreglo[i];
-        }
+    if (valor > max) {
+      max = valor;
     }
-    console.log(min, max);
+    arreglo.push(valor);
+    y++;
+  }
+  let min = arreglo[0];
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] <= min) {
+      min = arreglo[i];
+    }
+  }
+  console.log(min, max);
 }
 
-miniMaxSum([1, 3, 5, 7, 9]);
+// miniMaxSum([1, 3, 5, 7, 9]);
+
+const multiplayIntegers = (array) => {
+  let max = 0;
+  let product = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      for (let k = j; k < array.length; k++) {
+        product = array[i] * array[j] * array[k];
+        product > max ? (max = product) : max;
+      }
+    }
+  }
+
+  return max;
+};
+
+console.log(multiplayIntegers([-10, -10, 5, 2]));
---
+++ code/whatIsInAName.js
@@ -0,0 +1,22 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou
+
+const whatIsInAName = (collection, source) => {
+  Object.entries(source).map(([key, value]) => {
+    for (let i = 0; i < collection.length; i++) {
+      if (key in collection[i] && value == collection[i][key]) {
+        console.log('Lenin', collection[i][key], key);
+      }
+    }
+  });
+};
+
+// whatIsInAName(
+//   [
+//     { first: 'Romeo', last: 'Montague' },
+//     { first: 'Mercutio', last: null },
+//     { first: 'Tybalt', last: 'Capulet' },
+//   ],
+//   { last: 'Capulet' }
+// );
+
+whatIsInAName([{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }], { apple: 1, bat: 2 });
