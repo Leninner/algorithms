9bcf21df2b2e1617d2c6fd5af413377084c3c943
leninner <mazabandalenin180@gmail.com>
2022-02-13 21:03:20 -0500

    Update project

--- code/maxSequence.js
+++ code/maxSequence.js
@@ -24,4 +24,20 @@ const maxSequence = (arr) => {
   return Math.max(...arreglo);
 };
 
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+const maxSequence2 = (arr) => {
+  let min = 0,
+    ans = 0,
+    sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    console.log(min);
+    ans = Math.max(ans, sum - min);
+    console.log(ans);
+  }
+
+  return ans;
+};
+
+console.log(maxSequence2([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
