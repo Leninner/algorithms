77de272634f3cbb3b26f933d9e2a130323d73092
leninner <mazabandalenin180@gmail.com>
2022-02-19 18:09:49 -0500

    Added more Typescript

---
+++ code_ts/build/module01.js
@@ -0,0 +1,5 @@
+"use strict";
+function addNumbers(x, y) {
+    return x + y;
+}
+console.log(addNumbers(3, 6));
---
+++ code_ts/module01.ts
@@ -0,0 +1,5 @@
+function addNumbers(x:number, y:number) {
+  return x + y;
+}
+
+console.log(addNumbers(3, 6));
