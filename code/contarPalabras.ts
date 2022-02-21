7071c1a4e3312686c50cd14123728d377f6f68a7
leninner <mazabandalenin180@gmail.com>
2022-02-20 21:50:59 -0500

    Contar Palabras TS

---
+++ code/contarPalabras.ts
@@ -0,0 +1,5 @@
+// Make a function that can count the number of words in a string
+
+const contarPalabrasTS = (string: string) => string.split(' ').length;
+
+console.log(contarPalabrasTS('hola mundo'));
