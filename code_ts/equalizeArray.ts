88494fa56e683ba4158615852e9bec4ad3885832
leninner <mazabandalenin180@gmail.com>
2022-03-14 17:14:00 -0500

    Update

--- code_ts/equalizeArray.ts
+++ code_ts/equalizeArray.ts
@@ -8,21 +8,18 @@
 const equalizeArray = (arr: number[]): number => {
   let obj: { [key: number]: number } = {};
 
-  for (let i = 0; i < arr.length; i++) {
-    if (obj[arr[i]]) {
-      obj[arr[i]]++;
-    } else {
-      obj[arr[i]] = 1;
-    }
-  }
+  // Solution hermosa
+  arr.forEach((num: number) => {
+    obj[num] = (obj[num] || 0) + 1;
+  });
 
   const result = Object.entries(obj)
     .sort((a, b) => b[1] - a[1])
     .slice(1)
-    .reduce((acc, [key, value]) => (acc += value), 0);
+    .reduce((acc, [key, value]) => acc + value, 0);
 
   return result;
 };
 
 console.log(equalizeArray([1, 2, 2, 3]));
-console.log(equalizeArray([3, 3, 2, 1, 3]));
+console.log(equalizeArray([3, 3, 2, 1, 3, 1, 1, 1, 1, 1, 1, 1]));
