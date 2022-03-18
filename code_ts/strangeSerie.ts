8051b82ecb9d6e7914388eba2394607b7900e526
leninner <mazabandalenin180@gmail.com>
2022-03-18 11:27:53 -0500

    Update test cases

--- code_ts/strangeSerie.ts
+++ code_ts/strangeSerie.ts
@@ -1,3 +1,11 @@
+/**
+ *
+ * @param {number} a Primer número
+ * @param {number} b Segundo número
+ * @param {number} n Número final de exponentes de 2
+ * @link https://www.hackerrank.com/challenges/java-loops/problem?h_r=profile
+ */
+
 const strangeSerie = (a: number, b: number, n: number) => {
   let aux: number = 0;
 
@@ -14,5 +22,5 @@ const strangeSerie = (a: number, b: number, n: number) => {
 
 strangeSerie(0, 2, 10); // 2 6 14 30 62 126 254 510 1022 2046
 strangeSerie(1, 2, 10); // 3 7 15 31 63 127 255 511 1023 2047
-strangeSerie(1, 3, 10); // 4 8 16 32 64 128 256 512 1024 2048 4096
-strangeSerie(5, 3, 5); // 15 21 29
+strangeSerie(1, 3, 10); // 4 10 22 46 94 190 382 766 1534 3070
+strangeSerie(5, 3, 5); // 8 14 26 50 98
