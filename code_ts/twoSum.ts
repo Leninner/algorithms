a0f22c27c7d81a8c2632b02dfc62b452bd066630
leninner <mazabandalenin180@gmail.com>
2022-03-19 22:51:09 -0500

    Solución añadida a Two Sum en tiempo N y espacio N

---
+++ code_ts/twoSum.ts
@@ -0,0 +1,15 @@
+const twoSum = (arrayNumbers: number[], targetNumber: number): string | number[] => {
+  let results: any = {};
+
+  for (let i = 0; i < arrayNumbers.length; i++) {
+    results[arrayNumbers[i]] = i;
+
+    if (targetNumber - arrayNumbers[i] in results) {
+      return [results[targetNumber - arrayNumbers[i]], i];
+    }
+  }
+
+  return 'No se encontró la suma';
+};
+
+console.log(twoSum([2, 7, 11, 15], 8)); // [0, 1]
