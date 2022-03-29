5a6d621e5b050532d95e07158e046374d4c29bf5
leninner <mazabandalenin180@gmail.com>
2022-03-29 11:32:46 -0500

    Update

--- code_ts/lonelyInteger.ts
+++ code_ts/lonelyInteger.ts
@@ -4,8 +4,6 @@
  * @returns {number} the number that occurs only one time
  */
 
-function lonelyinteger(a: number[]): number {
-  return a.find((value) => a.indexOf(value) === a.lastIndexOf(value)) || -1;
-}
+const lonelyinteger = (a: number[]): number => a.find((value) => a.indexOf(value) === a.lastIndexOf(value)) || -1;
 
 console.log(lonelyinteger([1, 1, 4, 5, 4]));
