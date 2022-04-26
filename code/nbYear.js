2db0b05d5f544b0170bc940d98c996f448367c5e
leninner <mazabandalenin180@gmail.com>
2022-04-25 20:56:54 -0500

    Add nbYear solution

---
+++ code/nbYear.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param {*} p0
+ * @param {*} percent
+ * @param {*} aug
+ * @param {*} p
+ * @returns number of years to reach the population
+ * @link https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
+ */
+
+const nbYear = (p0, percent, aug, p) => {
+  let newPercent = percent / 100,
+    initialPoblation = p0,
+    year = 0;
+
+  while (initialPoblation < p) {
+    initialPoblation += parseInt(initialPoblation * newPercent + aug);
+    year++;
+  }
+
+  year = initialPoblation > p ? year++ : year;
+  return year;
+};
