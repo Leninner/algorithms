ee08ecd4b0df492fd1505d1fec9724f7ef30493b
leninner <mazabandalenin180@gmail.com>
2022-07-27 22:33:17 -0500

    Update project

--- code_ts/validParentheses.ts
+++ code_ts/validParentheses.ts
@@ -1,24 +1,20 @@
-function isValid(s: string): boolean {
-  const brackets: any = {
-    '(': ')',
-    '{': '}',
-    '[': ']',
-  }
-
-  if (Object.values(brackets).includes(s[0]) || s.length % 2) return false
-
+function isValid(brackets: string): boolean {
   let stack: string[] = []
 
-  for (let i = 0; i < s.length; i++) {
-    if (brackets[s[i]]) stack.push(s[i])
-    else if (brackets[stack[stack.length - 1]] === s[i]) stack.pop()
-    else return false
+  for (let bracket of brackets) {
+    if (stack.length && isClosing(stack[stack.length - 1], bracket)) stack.pop()
+    else stack.push(bracket)
   }
 
-  return true
+  return !stack.length
+}
+
+function isClosing(open: String, close: String) {
+  return (open === '(' && close === ')') || (open === '{' && close === '}') || (open === '[' && close === ']')
 }
 
 console.log(isValid('(')) // false
 console.log(isValid('())')) // false
 console.log(isValid('((({{{}}})))')) // true
 console.log(isValid('((({}})))()()')) // false
+console.log(isValid('())({}}{()][][')) //false
