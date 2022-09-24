ae1723488cea36e5a7766c4e543f564c1cd2a4f5
leninner <lenin@tinkin.one>
2022-09-23 23:02:31 -0500

    Add solution for the 05 advent js challenge´s solution

---
+++ code_ts/adventJs/05.ts
@@ -0,0 +1,49 @@
+interface IMonthDays {
+  month: string
+  days: number
+}
+
+const monthDays: IMonthDays[] = [
+  { month: 'Jan', days: 31 },
+  { month: 'Feb', days: 28 },
+  { month: 'Mar', days: 31 },
+  { month: 'Apr', days: 30 },
+  { month: 'May', days: 31 },
+  { month: 'Jun', days: 30 },
+  { month: 'Jul', days: 31 },
+  { month: 'Aug', days: 31 },
+  { month: 'Sep', days: 30 },
+  { month: 'Oct', days: 31 },
+  { month: 'Nov', days: 30 },
+  { month: 'Dec', days: 31 },
+]
+
+const CHRISTMAS_DATE = new Date('Dec 25, 2021')
+const DAYS_FROM_CHRISTMAS_TO_END_YEAR = 31 - CHRISTMAS_DATE.getDate()
+const DAYS_YEAR = 365
+
+export const countingTheDaysBeforeChristmas = (date: Date) => {
+  const isChristmas = date === CHRISTMAS_DATE
+  const isDecember = date.getMonth() === CHRISTMAS_DATE.getMonth()
+  const isBeforeChristmas = date < CHRISTMAS_DATE
+  const daysInMonth = monthDays[date.getMonth()].days
+  const daysLeftInMonth = daysInMonth - date.getDate()
+
+  if (isChristmas) return 0
+  if (isDecember) return CHRISTMAS_DATE.getDate() - date.getDate()
+  if (isBeforeChristmas) {
+    const daysLeftInYear = monthDays
+      .slice(date.getMonth() + 1)
+      .reduce(
+        (acc: number, { days }: Omit<IMonthDays, 'month'>) => acc + days,
+        0
+      )
+
+    return daysLeftInMonth + daysLeftInYear - DAYS_FROM_CHRISTMAS_TO_END_YEAR
+  }
+
+  const daysLeftInYear = monthDays
+    .slice(date.getMonth() + 1)
+    .reduce((acc, { days }) => acc - days, DAYS_YEAR)
+  return daysLeftInMonth - daysLeftInYear - DAYS_FROM_CHRISTMAS_TO_END_YEAR
+}
---
+++ tests/adventJs/05.test.ts
@@ -0,0 +1,47 @@
+import { countingTheDaysBeforeChristmas } from '../../code_ts/adventJs/05'
+
+describe('Counting the days before Christmas tests', () => {
+  describe('The function', () => {
+    it('should be defined', () => {
+      expect(countingTheDaysBeforeChristmas).toBeDefined()
+    })
+
+    it('should acept an instance of a Date', () => {
+      const date = new Date()
+      expect(countingTheDaysBeforeChristmas(date)).toBeTruthy()
+    })
+
+    it('should return a int value', () => {
+      const date = new Date()
+      expect(typeof countingTheDaysBeforeChristmas(date)).toBe('number')
+    })
+  })
+
+  describe("The function's logic", () => {
+    it('should return 0 if the date is Christmas', () => {
+      const date = new Date('Dec 25, 2021')
+      expect(countingTheDaysBeforeChristmas(date)).toBe(0)
+    })
+
+    it("if the date isn't Christmas, it should return the number of days before Christmas", () => {
+      expect(
+        countingTheDaysBeforeChristmas(new Date('December 24, 2021 03:24:00'))
+      ).toBe(1)
+      expect(countingTheDaysBeforeChristmas(new Date('Dec 10, 2021'))).toBe(15)
+      expect(countingTheDaysBeforeChristmas(new Date('Nov 1, 2021'))).toBe(54)
+      expect(countingTheDaysBeforeChristmas(new Date('Jan 1, 2021'))).toBe(358)
+    })
+
+    it('if the date is after Christmas, it should return the number of days before Christmas of the next year', () => {
+      expect(countingTheDaysBeforeChristmas(new Date('Dec 26, 2021'))).toBe(-1)
+      expect(countingTheDaysBeforeChristmas(new Date('Dec 31, 2021'))).toBe(-6)
+      expect(
+        countingTheDaysBeforeChristmas(new Date('Jan 1, 2022 23:59:59'))
+      ).toBe(-7)
+      expect(
+        countingTheDaysBeforeChristmas(new Date('Jan 1, 2022 00:00:01'))
+      ).toBe(-7)
+      expect(countingTheDaysBeforeChristmas(new Date('Dec 26, 2021'))).toBe(-1)
+    })
+  })
+})
