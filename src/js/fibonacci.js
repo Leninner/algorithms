cf92eaace0e15826f8ae461ca99036fcfa3a9e91
leninner <mazabandalenin180@gmail.com>
2022-10-12 17:36:17 -0500

    WIP Update

--- src/js/fibonacci.js
+++ src/js/fibonacci.js
@@ -1,33 +1,33 @@
 // calcular  la serie de fibonaacci  e ir imprimiéndola en la pantalla
 
-// export const fibonacci = (n) => {
-//   let a = 1
-//   let b = 0
-//   let curr = 0
+export const fibonacci = (n) => {
+  let a = 1
+  let b = 0
+  let curr = 0
 
-//   let i = 0
-//   while (i < n) {
-//     curr = a + b
-//     a = b
-//     b = curr
-//     i++
-//   }
-// }
+  let i = 0
+  while (i < n) {
+    curr = a + b
+    a = b
+    b = curr
+    i++
+  }
+}
 
-// export const fibonacciRevisited = (n) => {
-//   if (n === 0 || n === 1) return n
-//   let a = 1
-//   let b = 0
-//   let c
+export const fibonacciRevisited = (n) => {
+  if (n === 0 || n === 1) return n
+  let a = 1
+  let b = 0
+  let c
 
-//   for (let i = 0; i < n; i++) {
-//     c = a + b
-//     a = b
-//     b = c
-//   }
+  for (let i = 0; i < n; i++) {
+    c = a + b
+    a = b
+    b = c
+  }
 
-//   return c
-// }
+  return c
+}
 
 // calcular la serie de  fibonacci e ir almacenando  los resultados en un arreglo
 
@@ -47,18 +47,18 @@ export const fibonacciInArray = (n) => {
   return array
 }
 
-// // Serie de fibonacci con recursividad
+// Serie de fibonacci con recursividad
 
-// export const fibonacciRecursive = (n) => {
-//   if (n === 0 || n === 1) return n
-//   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
-// }
+export const fibonacciRecursive = (n) => {
+  if (n === 0 || n === 1) return n
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+}
 
-// // Recursividad con memoization
+// Recursividad con memoization
 
-// export const fibonacciMemoization = (n, memo = []) => {
-//   if (memo[n] !== undefined) return memo[n]
-//   if (n === 0 || n === 1) return n
-//   return (memo[n] =
-//     fibonacciMemoization(n - 1, memo) + fibonacciMemoization(n - 2, memo))
-// }
+export const fibonacciMemoization = (n, memo = []) => {
+  if (memo[n] !== undefined) return memo[n]
+  if (n === 0 || n === 1) return n
+  return (memo[n] =
+    fibonacciMemoization(n - 1, memo) + fibonacciMemoization(n - 2, memo))
+}
