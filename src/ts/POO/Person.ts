30edcd4feb5a0c1932d6db07709f83ad360f0087
leninner <lenin@tinkin.one>
2022-10-20 17:43:36 -0500

    Update Person class and Person tests

--- src/ts/POO/Person.ts
+++ src/ts/POO/Person.ts
@@ -1,19 +1,46 @@
+type SexType = 'H' | 'M'
+
 export class Person {
-  public name: string
-  public lastName: string
-  public age: number
+  private name: string
+  private lastName: string
+  private documentId: string
+  private sex: SexType
+  private weight: number
+  private height: number
+  private age: number
 
-  constructor(name: string, lastName: string, age: number) {
-    this.name = name
-    this.lastName = lastName
-    this.age = age < 0 ? 0 : age
+  constructor(
+    documentId: string,
+    name?: string,
+    lastName?: string,
+    sex?: 'M' | 'H',
+    weight?: number,
+    height?: number,
+    age?: number
+  ) {
+    this.documentId = documentId
+    this.name = name || ''
+    this.lastName = lastName || ''
+    this.sex = sex || 'H'
+    this.weight = weight || 0
+    this.height = height || 0
+    this.age = age || 0
   }
 
-  public walk(): string {
-    return `${this.name} ${this.lastName} is walking`
+  public calculateIMC(): number {
+    const IMC = this.weight / this.height ** 2
+
+    if (IMC > 25) return 1
+    if (IMC >= 20 && IMC <= 25) return 0
+
+    return -1
   }
 
-  public talk(): string {
-    return `${this.name} ${this.lastName} is talking`
+  public isOlder(): boolean {
+    return this.age > 17
+  }
+
+  public toString(): string {
+    return `Name: ${this.name}, LastName: ${this.lastName}, DocumentId: ${this.documentId}, Sexo: ${this.sex}, Weight: ${this.weight}, Height: ${this.height}, Age: ${this.age}`
   }
 }
--- src/ts/POO/tests/Person.test.ts
+++ src/ts/POO/tests/Person.test.ts
@@ -1,26 +1,97 @@
 import { Person } from '../Person'
 
 describe('Person class tests', () => {
-  it('should create a person with name, lastName and age', () => {
-    const person = new Person('John', 'Doe', 30)
-    expect(person.name).toBe('John')
-    expect(person.lastName).toBe('Doe')
-    expect(person.age).toBe(30)
+  it('should create a person with name, lastName, age, documentId, sexo (H/M), weight and height', () => {
+    const person = new Person('123456789', 'John', 'Doe', 'M', 80, 1.8, 18)
+    expect(person).toEqual({
+      documentId: '123456789',
+      name: 'John',
+      lastName: 'Doe',
+      height: 1.8,
+      weight: 80,
+      sex: 'M',
+      age: 18,
+    })
     expect(person).toBeInstanceOf(Person)
   })
 
-  it("The person can't have a negative age", () => {
-    const person = new Person('John', 'Doe', -30)
-    expect(person.age).toBe(0)
+  it('a person can be created only with documentId', () => {
+    const person = new Person('123456789')
+    expect(person).toEqual({
+      documentId: '123456789',
+      name: '',
+      lastName: '',
+      height: 0,
+      weight: 0,
+      sex: 'H',
+      age: 0,
+    })
   })
 
-  it('The person can walk', () => {
-    const person = new Person('John', 'Doe', 30)
-    expect(person.walk()).toBe('John Doe is walking')
+  describe('When the calculateIMC is called', () => {
+    it("should return '1' when the IMC is greater than 25", () => {
+      const personWithObesity = new Person(
+        '123456789',
+        'John',
+        'Doe',
+        'M',
+        80,
+        1.65
+      )
+
+      expect(personWithObesity.calculateIMC()).toBe(1)
+    })
+
+    it('should return 0 when the IMC is between 20 and 25', () => {
+      const personWithCorrectIMC = new Person(
+        '123456789',
+        'John',
+        'Doe',
+        'M',
+        80,
+        1.8
+      )
+
+      expect(personWithCorrectIMC.calculateIMC()).toBe(0)
+    })
+
+    it('should return -1 when the IMC is less than 20', () => {
+      const personWithFrailty = new Person(
+        '1850994623',
+        'Lenin',
+        'Dio',
+        'M',
+        20,
+        1.8
+      )
+
+      expect(personWithFrailty.calculateIMC()).toBe(-1)
+    })
   })
 
-  it('The person can talk', () => {
-    const person = new Person('John', 'Doe', 30)
-    expect(person.talk()).toBe('John Doe is talking')
+  describe('When the isOlder is called', () => {
+    it('should return true if the age is greater than 17', () => {
+      const personWithCorrectAge = new Person(
+        '1850994623',
+        'Lenin',
+        'Dio',
+        'M',
+        89,
+        1.8,
+        18
+      )
+
+      expect(personWithCorrectAge.isOlder()).toBeTruthy()
+    })
+  })
+
+  describe('When the toString method is called', () => {
+    it("should return all the info of the person's object", () => {
+      const person = new Person('1850994623', 'Lenin', 'Dio', 'M', 89, 1.8, 18)
+
+      expect(person.toString()).toBe(
+        `Name: Lenin, LastName: Dio, DocumentId: 1850994623, Sexo: M, Weight: 89, Height: 1.8, Age: 18`
+      )
+    })
   })
 })
