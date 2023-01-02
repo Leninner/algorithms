1518cfbe8364dee78ea8fc5d3975169723df98c2
leninner <mazabandalenin180@gmail.com>
2023-01-01 19:53:16 -0500

    Add formula`s description

--- src/ts/projectEuler/04.ts
+++ src/ts/projectEuler/04.ts
@@ -1,5 +1,16 @@
+/**
+ *
+ * @description Gauss' formula for sum of first n natural numbers
+ * @param n number
+ * @returns number
+ */
 export const getPlainSumTillN = (n: number): number => (n * (n + 1)) / 2
 
+/**
+ * @description Formula for sum the squares of first n natural numbers
+ * @param n number
+ * @returns number
+ */
 export const getDoubleSumTillN = (n: number): number =>
   (n * (n + 1) * (2 * n + 1)) / 6
 
