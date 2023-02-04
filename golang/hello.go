d157f47392bc4d55f0710e155283d270209ff303
leninner <mazabandalenin180@gmail.com>
2023-02-03 20:39:38 -0500

    Learning GO

--- golang/hello.go
+++ golang/hello.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	result := sum.Sum(5, 5)
+	hello := "Leninsin"
+	singleCharacter := 'a'
 	fmt.Println(result)
+	fmt.Println(hello)
+	fmt.Println(singleCharacter, "is a single character")
 }
