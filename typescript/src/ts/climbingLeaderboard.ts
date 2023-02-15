e7b7175bd70256948624c0142107904d28d75d18
leninner <mazabandalenin180@gmail.com>
2023-02-14 23:40:09 -0500

    update climbing leaderboard

--- typescript/src/ts/climbingLeaderboard.ts
+++ typescript/src/ts/climbingLeaderboard.ts
@@ -2,13 +2,41 @@ export const climbingLeaderboard = (
   ranked: number[],
   player: number[]
 ): number[] => {
+  const rankedWithUniquePositions = [...new Set(ranked)]
   const result: number[] = []
-  const rankedUniques: number[] = [...new Set(ranked)]
 
-  for (let i = 0; i < player.length; i++) {
-    rankedUniques.push(player[i])
-    result.push(rankedUniques.sort((a, b) => b - a).indexOf(player[i]) + 1)
-    rankedUniques.splice(rankedUniques.indexOf(player[i]), 1)
+  const playerLength = player.length
+  const rankedLength = rankedWithUniquePositions.length
+
+  let temporalPositionPlayer = 0
+  let temporalPositionRanked = rankedLength - 1
+
+  for (let i = temporalPositionPlayer; i < playerLength; i++) {
+    if (player[i] === player[i + 1]) continue
+    for (let y = temporalPositionRanked; y >= 0; y--) {
+      if (player[i] < rankedWithUniquePositions[y]) {
+        if (player[i] === rankedWithUniquePositions[y + 1]) {
+          result.push(y + 2)
+          temporalPositionRanked = y
+          temporalPositionPlayer = i + 1
+          break
+        }
+
+        rankedWithUniquePositions.splice(y + 1, 0, player[i])
+        result.push(y + 2)
+        temporalPositionRanked = y
+        temporalPositionPlayer = i + 1
+        break
+      }
+
+      if (player[i] >= rankedWithUniquePositions[0]) {
+        rankedWithUniquePositions.splice(0, 0, player[i])
+        result.push(1)
+        temporalPositionRanked = y
+        temporalPositionPlayer = i + 1
+        break
+      }
+    }
   }
 
   return result
--- typescript/src/ts/tests/climbingLeaderboard.test.ts
+++ typescript/src/ts/tests/climbingLeaderboard.test.ts
@@ -1,26 +1,31 @@
 import { climbingLeaderboard } from '../climbingLeaderboard'
 
 describe('climbingLeaderboard', () => {
-  describe('When the ranked scores are [100,80,80,50]', () => {
-    const ranked = [100, 80, 80, 50]
+  describe('When function is called', () => {
+    const ranked = [100, 90, 90, 80]
+    const player = [70, 80, 105]
 
-    it('should return 3 if the first position in the player´s score is 60', () => {
-      const player = [60]
-      const result = climbingLeaderboard(ranked, player)
-      expect(result).toEqual([3])
+    it('should return an array with the same length of the player`s array', () => {
+      expect(climbingLeaderboard(ranked, player)).toHaveLength(3)
     })
 
-    it('should return [3, 1] if the second position of the player´s score is 105', () => {
-      const player = [60, 105]
-      const result = climbingLeaderboard(ranked, player)
-      expect(result).toEqual([3, 1])
-    })
-
-    it('should work with the example cases', () => {
+    it('after each game, the result array should be filled', () => {
       const ranked = [100, 90, 90, 80]
       const player = [70, 80, 105]
-      const result = climbingLeaderboard(ranked, player)
-      expect(result).toEqual([4, 3, 1])
+
+      expect(climbingLeaderboard(ranked, player)).toEqual([4, 3, 1])
+    })
+
+    it('should work with the challenge example', () => {
+      const ranked = [100, 100, 50, 40, 40, 20, 10]
+      const player = [5, 25, 50, 120]
+
+      const rankedTwo = [100, 90, 90, 80, 75, 60]
+      const playerTwo = [50, 65, 77, 90, 102]
+
+      expect(climbingLeaderboard(ranked, player)).toEqual([6, 4, 2, 1])
+
+      expect(climbingLeaderboard(rankedTwo, playerTwo)).toEqual([6, 5, 4, 2, 1])
     })
   })
 })
