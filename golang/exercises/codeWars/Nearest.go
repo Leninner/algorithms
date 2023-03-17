6d9ba47360160dc3720c076a4517e4767029ce82
leninner <mazabandalenin180@gmail.com>
2023-03-16 22:06:52 -0500

    add method description

--- golang/exercises/codeWars/Nearest.go
+++ golang/exercises/codeWars/Nearest.go
@@ -11,6 +11,15 @@ func NearestSq(n int) int {
 		return n
 	}
 
+	/*
+		La expresión se compone de dos partes:
+
+		(base+1)*(base+1)-n: esto calcula la diferencia entre el cuadrado perfecto más cercano que es mayor que n y n.
+		n-(base*base): esto calcula la diferencia entre n y el cuadrado perfecto más cercano que es menor o igual que n.
+
+		La condición se cumple si la primera diferencia es menor que la segunda diferencia. En otras palabras, esto significa que (base+1)*(base+1) está más cerca de n que base*base. Si se cumple esta condición, entonces se devuelve (base+1)*(base+1) como el cuadrado perfecto más cercano a n.
+	*/
+
 	if (base+1)*(base+1)-n < n-(base*base) {
 		return (base + 1) * (base + 1)
 	}
