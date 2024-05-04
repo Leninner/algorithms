803a9b8c274f57043f5252ba2c7b9473b0a64bca
leninner <mazabandalenin180@gmail.com>
2024-05-04 12:36:09 -0500

    Add solution to leetcode # 12

---
+++ golang/exercises/leetcode/integer-to-roman/integer-to-roman.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Leetcode #12 Integer to Roman | https://leetcode.com/problems/integer-to-roman/description/
+func IntegerToRoman(number int) string {
+	factor := 1
+	result := ""
+
+	romanNumbers := map[int]string{
+		1:    "I",
+		4:    "IV", // split from one character to another
+		5:    "V",
+		9:    "IX", // split from one character to another
+		10:   "X",
+		40:   "XL", // split from one character to another
+		50:   "L",
+		90:   "XC", // split from one character to another
+		100:  "C",
+		400:  "CD", // split from one character to another
+		500:  "D",
+		900:  "CM", // split from one character to another
+		1000: "M",
+	}
+
+	if romanNumbers[number] != "" {
+		return romanNumbers[number]
+	}
+
+	for number > 0 {
+		digit := number % 10
+		number /= 10
+
+		// when digit is 3 or less
+		if digit < 4 {
+			result = strings.Repeat(romanNumbers[factor], digit) + result
+		}
+
+		// when digit is 4 or 9
+		if digit == 4 || digit == 9 {
+			result = romanNumbers[digit*factor] + result
+		}
+
+		// when digit is 5 or 6 or 7 or 8
+		if digit > 4 && digit < 9 {
+			result = romanNumbers[5*factor] + strings.Repeat(romanNumbers[factor], digit-5) + result
+		}
+
+		factor *= 10
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(IntegerToRoman(15))   // XV
+	fmt.Println(IntegerToRoman(16))   // XVI
+	fmt.Println(IntegerToRoman(19))   // XVI
+	fmt.Println(IntegerToRoman(29))   // XVI
+	fmt.Println(IntegerToRoman(500))  // D
+	fmt.Println(IntegerToRoman(129))  // CXXIX
+	fmt.Println(IntegerToRoman(3749)) // MMMDCCXLIX
+	fmt.Println(IntegerToRoman(999))  // CMXCIX
+}
---
+++ golang/exercises/leetcode/integer-to-roman/integer_to_roman_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestIntegerToRoman(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    int
+		expected string
+	}{
+		{"Test case 1", 3, "III"},
+		{"Test case 2", 4, "IV"},
+		{"Test case 3", 9, "IX"},
+		{"Test case 4", 58, "LVIII"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IntegerToRoman(tt.input)
+
+			if result != tt.expected {
+				t.Errorf("got %s, want %s", result, tt.expected)
+			}
+		})
+	}
+}
